c70da10a1013451e051f87ba641512c3dfc60fc3
magno309 <luisabrahamsanabriamelecio@gmail.com>
2022-06-02 13:03:55 -0500

    Se modifico una prueba

--- src/test/java/com/sistema/inventario/CategoriaRepositoryTest.java
+++ src/test/java/com/sistema/inventario/CategoriaRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.sistema.inventario;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
